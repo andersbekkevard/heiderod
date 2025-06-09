b96ec4f56db7e86be0b889a45057e240b747ab82
Anders Bekkevard <andersbekkevard@anders-sin-mbp.lan>
2025-06-09 13:50:28 +0200

    fix images 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	base: '/heiderod/',
 	plugins: [react()],
+	build: {
+		assetsDir: 'assets',
+		rollupOptions: {
+			output: {
+				assetFileNames: 'assets/[name].[hash][extname]'
+			}
+		}
+	},
+	publicDir: 'public'
 });
