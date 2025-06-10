4b71ade181b42aeb703034428581a294501819d1
Anders Bekkevard <anders.bekkevard@gmail.com>
2025-06-10 23:12:58 +0200

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	base: '/heiderod/',
+	base: '/',
 	plugins: [react()],
 	build: {
 		assetsDir: 'assets',
